2d6d55c4a95f21a72f09fcf2849708899dbc41aa
Aleksi Tarasov <alexey2g@yandex.ru>
2021-10-06 23:14:24 +0300

    add cors with def options

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const express = require('express');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const { errors } = require('celebrate');
 
 const { createUser, login } = require('./controllers/users');
 const { signInValidation, signUpValidation } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger')
 const auth = require('./middlewares/auth');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
@@ -25,14 +27,17 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 app.use(helmet());
 app.use(cookieParser());
+app.use(requestLogger)
 app.post('/signin', signInValidation, login);
 app.post('/signup', signUpValidation, createUser);
 app.use(auth);
 app.use('/', usersRouter);
 app.use('/', cardsRouter);
+app.use(errorLogger)
 app.use('*', () => { throw new NotFoundError('Запрашиваемый адрес не найден'); });
 app.use(errors());
 app.use((error, req, res, next) => {
